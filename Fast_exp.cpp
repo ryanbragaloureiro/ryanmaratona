4acf9f8e27490a0c5ad1e9740e488e935d457d7a
ryanbragaloureiro <ryanbloureiro@gmail.com>
2025-11-14 13:07:04 -0300

    Create Fast_exp.cpp

---
+++ Fast_exp.cpp
@@ -0,0 +1,14 @@
+// fexp + inverso modular
+ll fexp(ll b, ll e){
+    ll res=1;
+    while(e){
+        if(e&1) res = (res*b)%MOD // caso não tenha mod, tirar
+        b = (b*b)%MOD;
+        e/=2;
+    }
+    return res;
+}
+
+ll inv(ll x){
+    return fexp(x,MOD-2); // MOD deve ser primo
+}
